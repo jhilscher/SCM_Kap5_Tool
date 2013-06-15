84af6ab588e1f212f6c2fd755a28161eb5b9b43b
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-06-15 10:49:58 +0200

    1111

--- ToolFahrrad-v1/Windows/TeilInformation.cs
+++ ToolFahrrad-v1/Windows/TeilInformation.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ToolFahrrad_v1
+namespace ToolFahrrad_v1.Windows
 {
     public partial class TeilInformation : Form
     {
         // Get current language
-        private String culInfo = Thread.CurrentThread.CurrentUICulture.Name;
+        private String _culInfo = Thread.CurrentThread.CurrentUICulture.Name;
         // -----------------------------------------------------------------
         private const String de = "de";
         private const String deKTeil = "Kaufteil Nr.: ";
@@ -50,22 +44,22 @@ namespace ToolFahrrad_v1
             // Set label to ordering part
             if (name.Equals("kteil"))
             {
-                label1.Text = culInfo.Contains(de) ? deKTeil + _nummer : enKTeil + _nummer;
+                label1.Text = _culInfo.Contains(de) ? deKTeil + _nummer : enKTeil + _nummer;
             }
             // Set label to working station
             else if (name.Equals("arbeitsplatz"))
             {
-                label1.Text = culInfo.Contains(de) ? deAPlatz + _nummer : enAPlatz + _nummer;
+                label1.Text = _culInfo.Contains(de) ? deAPlatz + _nummer : enAPlatz + _nummer;
             }
         }
         internal void GetTeilvonETeilMitMenge()
         {
             ausgabe.Text =
-                (culInfo.Contains(de) ? deLDauer : enLDauer) + (dc.GetTeil(_nummer) as KTeil).Lieferdauer
+                (_culInfo.Contains(de) ? deLDauer : enLDauer) + (dc.GetTeil(_nummer) as KTeil).Lieferdauer
                 + " (" + (dc.GetTeil(_nummer) as KTeil).AbweichungLieferdauer + ")\n"
-                + (culInfo.Contains(de) ? deDMenge : enDMenge)
-                + (dc.GetTeil(_nummer) as KTeil).DiskontMenge + "\n" + (culInfo.Contains(de) ? dePreis : enPreis)
-                + (dc.GetTeil(_nummer) as KTeil).Preis + "\n" + (culInfo.Contains(de) ? deBKosten : enBKosten)
+                + (_culInfo.Contains(de) ? deDMenge : enDMenge)
+                + (dc.GetTeil(_nummer) as KTeil).DiskontMenge + "\n" + (_culInfo.Contains(de) ? dePreis : enPreis)
+                + (dc.GetTeil(_nummer) as KTeil).Preis + "\n" + (_culInfo.Contains(de) ? deBKosten : enBKosten)
                 + (dc.GetTeil(_nummer) as KTeil).Bestellkosten + "\n\n";
             List<ETeil> list = (dc.GetTeil(_nummer) as KTeil).IstTeilVon;
             int sum = 0;
@@ -88,7 +82,7 @@ namespace ToolFahrrad_v1
                                 pm = e.ProduktionsMengePer0;
                             }
                             ausgabe.Text
-                                += (culInfo.Contains(de) ? deInTeil : enInTeil) + e.Nummer + "   " + kvp.Value
+                                += (_culInfo.Contains(de) ? deInTeil : enInTeil) + e.Nummer + "   " + kvp.Value
                                 + " * " + pm + " = " + kvp.Value * pm + "\n";
                             sum += kvp.Value * pm;
                         }
@@ -99,7 +93,7 @@ namespace ToolFahrrad_v1
                                 val += pair.Value;
                             }
                             ausgabe.Text
-                                += (culInfo.Contains(de) ? deInTeil : enInTeil) + e.Nummer + "  " + kvp.Value
+                                += (_culInfo.Contains(de) ? deInTeil : enInTeil) + e.Nummer + "  " + kvp.Value
                                 + " * " + val + " = " + kvp.Value * val + "\n";
                             sum += kvp.Value * val;
                             val = 0;
@@ -107,11 +101,11 @@ namespace ToolFahrrad_v1
                     }
                 }
             }
-            ausgabe.Text += (culInfo.Contains(de) ? deSumme : enSumme) + sum;
+            ausgabe.Text += (_culInfo.Contains(de) ? deSumme : enSumme) + sum;
         }
         internal void GetZeitInformation()
         {
-            ausgabe2.Text = (culInfo.Contains(de) ? deRZeit : enRZeit) + "\n";
+            ausgabe2.Text = (_culInfo.Contains(de) ? deRZeit : enRZeit) + "\n";
             int sum = 0;
             int val = 0;
             int prMenge = 0;
@@ -143,11 +137,11 @@ namespace ToolFahrrad_v1
                         }
                     }
                 }
-                ausgabe2.Text += (culInfo.Contains(de) ? deTeil : enTeil) + kvp.Key + ": " + val + " min. \n";
+                ausgabe2.Text += (_culInfo.Contains(de) ? deTeil : enTeil) + kvp.Key + ": " + val + " min. \n";
                 sum += val;
             }
-            ausgabe2.Text += (culInfo.Contains(de) ? deSumme : enSumme) + sum + "\n\n";
-            ausgabe.Text += (culInfo.Contains(de) ? deKBedarf : enKBedarf) + "\n";
+            ausgabe2.Text += (_culInfo.Contains(de) ? deSumme : enSumme) + sum + "\n\n";
+            ausgabe.Text += (_culInfo.Contains(de) ? deKBedarf : enKBedarf) + "\n";
             sum = 0;
             prMenge = 0;
             foreach (KeyValuePair<int, int> kvp in dc.GetArbeitsplatz(_nummer).Werk_zeiten)
@@ -169,13 +163,13 @@ namespace ToolFahrrad_v1
                         prMenge = 0;
                     }
                     ausgabe.Text
-                        += (culInfo.Contains(de) ? deTeil : enTeil) + kvp.Key + ": " + kvp.Value + " * " + prMenge
+                        += (_culInfo.Contains(de) ? deTeil : enTeil) + kvp.Key + ": " + kvp.Value + " * " + prMenge
                         + " = " + kvp.Value * prMenge + " min.\n";
                     sum += kvp.Value * prMenge;
                     prMenge = 0;
                 }
             }
-            ausgabe.Text += (culInfo.Contains(de) ? deSumme : enSumme) + sum + "\n\n";
+            ausgabe.Text += (_culInfo.Contains(de) ? deSumme : enSumme) + sum + "\n\n";
         }
     }
 }
