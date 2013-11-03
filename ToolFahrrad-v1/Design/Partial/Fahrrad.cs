c8c99c61534c207a2031fa81a4a9054f61c0e9c5
jHilscher <joerg.hilscher@gmail.com>
2013-11-03 18:46:57 +0100

    test

---
+++ ToolFahrrad-v1/Design/Partial/Fahrrad.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolFahrrad_v1.Design
+{
+    /// <summary>
+    /// Joerg's partial.
+    /// </summary>
+    partial class Fahrrad
+    {
+        /// <summary>
+        /// 1. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void nav_button_1_Click(object sender, EventArgs e)
+        {
+            tabs.SelectedTab = tab_xml;
+        }
+
+        /// <summary>
+        /// 2. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 3. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 4. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 5. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_5_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        /// <summary>
+        /// 6. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 7. Button links
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_nav_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
