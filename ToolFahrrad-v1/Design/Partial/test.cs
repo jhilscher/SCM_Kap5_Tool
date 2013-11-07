cd9e05472dd1da513ad8558079a04962ccb08302
Christian Roser <christian.roser@1und1.de>
2013-11-07 15:53:16 +0100

    i have created a command-line tool which gets all offers in new objects

--- ToolFahrrad-v1/Design/Partial/test.cs
+++ ToolFahrrad-v1/Design/Partial/test.cs
@@ -2,6 +2,8 @@ using System;
 using System.Net;
 using System.IO;
 using System.Xml;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class WebsiteChecker 
 {
@@ -16,7 +18,9 @@ public class WebsiteChecker
 			GetUrl(false, url, "market/market_set.html", container);
 			Authenticate(url + "market/", container);
 			String html = GetUrl(true, url, "market/marketinfo.jsp", container);
-			html = RemoveUnusedCrap(html);
+			String[] parsed = RemoveUnusedCrap(html);
+			
+			List<Gebot> offers = ParseTable(parsed[1]);	
 			
 		}
 		catch (Exception e) 
@@ -25,7 +29,8 @@ public class WebsiteChecker
 		}
 	}
 
-	public static String GetUrl(Boolean ok, String url, String path, CookieContainer container) {
+	public static String GetUrl(Boolean ok, String url, String path, CookieContainer container) 
+	{
 		url += path;
 		HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 		request.CookieContainer = container;
@@ -38,7 +43,8 @@ public class WebsiteChecker
 		return body;
 	}
 
-	public static CookieContainer Authenticate(String url, CookieContainer container) {
+	public static CookieContainer Authenticate(String url, CookieContainer container) 
+	{
 		String username = "kap5";
 		String password = "sperling";
 		url += String.Format("j_security_check?j_username={0}&j_password={1}",
@@ -51,11 +57,121 @@ public class WebsiteChecker
 		authRequest.Timeout = 360000;
 
 		authRequest.GetResponse();
-		return container;//.Add(response.Cookies);
+		return container;
 	}
 
-	public static String RemoveUnusedCrap(String html) {
-		Console.WriteLine(html);
-		return "";
+	public static String[] RemoveUnusedCrap(String html) 
+	{
+		List<String> plain = new List<String>();
+		String[] splitted = html.Split(new Char[] { '\n' });
+		Boolean inTable = false;
+		for ( int i = 0; i < splitted.Length; i++) 
+		{
+			if (!splitted[i].Trim().StartsWith("<") || splitted[i].Trim().StartsWith("<table width"))
+			{
+				continue;
+			}
+			if (splitted[i].Trim().StartsWith("<table")) 
+			{
+				inTable = true;
+			}
+			if (inTable) 
+			{
+				if (splitted[i].Trim().StartsWith("</table")) 
+				{
+					inTable = false;
+				}
+				plain.Add(splitted[i].Trim().Replace("<tr>", "").Replace("</tr>", "\n").Replace("</td>", "|").Replace("<td align=\"center\">", "").Replace("</th>", ""));
+			}
+			
+		}
+		String xml = "";
+		for(int i = 0; i < plain.Count; i++) 
+		{
+			if (!plain[i].Equals("\n")) 
+			{
+				if ( !plain[i].Equals(""))
+				{
+					xml += plain[i];
+				}
+			}
+		}
+
+		Regex reg = new Regex("(\\<)(.*?)(\\>)\\s*");
+		xml = reg.Replace(xml, "");
+		
+		String[] data = xml.Split(new Char[] {';'});
+		return data;
 	}
+
+	public static List<Gebot> ParseTable(String str)
+	{
+		List<Gebot> ret = new List<Gebot>();
+		String[] splitted = str.Split(new Char[] { '\n' });
+		
+		foreach(String line in splitted)
+		{
+			String[] singleLine = line.Split(new Char[] { '|' });
+			if (singleLine.Length > 1)
+			{
+				ret.Add(new Gebot(singleLine[1], singleLine[2], singleLine[3], singleLine[0]));
+			}
+		}
+
+		foreach(Gebot ges in ret)
+		{
+			Console.WriteLine(ges);
+		}
+		return ret;
+	}
+}
+
+public class Angebot
+{
+	public String article;
+	public String quantity;
+	public String price;
+
+	public Angebot(String nArticle, String nQuantity, String nPrice) 
+	{
+		this.article = nArticle;
+		this.quantity = nQuantity;
+		this.price = nPrice;
+	}
+
+	public override String ToString()
+	{
+		return String.Format("Artikel: {0}, mit der Menge {1} und dem Preis {2}!", this.article, this.quantity, this.price);
+	}
+}
+
+public class Gebot : Angebot
+{
+	public String seller;	
+
+	public Gebot(String nArticle, String nQuantity, String nPrice, String nSeller) : base(nArticle, nQuantity, nPrice) 
+	{
+		this.seller = nSeller;
+	}
+
+	public override String ToString() 
+	{
+		return String.Format("Verkäufer {0} verkauft ", this.seller) + "" + base.ToString();
+	}
+}
+
+public class Gesuch : Angebot
+{
+	public String requestor;	
+
+	public Gesuch(String nArticle, String nQuantity, String nPrice, String nRequestor) : base(nArticle, nQuantity, nPrice) 
+	{
+		this.requestor = nRequestor;
+	}
+
+	public override String ToString() 
+	{
+		return String.Format("Kunde {0} sucht ", this.requestor) + "" + base.ToString();
+	}
+	
 }
